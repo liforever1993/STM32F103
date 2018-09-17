ca1e8ee3ff4696b87971c140808f5863b4c6f647
liforever1993 <liforever1993@outlook.com>
2018-09-18 07:17:37 +0800

    First Version

---
+++ STM32F103/Inc/app/nfc.h
@@ -0,0 +1,14 @@
+/*
+ * nfc.h
+ *
+ *  Created on: 2018Äê9ÔÂ17ÈÕ
+ *      Author: lifor
+ */
+
+#ifndef APP_NFC_H_
+#define APP_NFC_H_
+
+void nfc_task();
+
+
+#endif /* APP_NFC_H_ */
---
+++ STM32F103/Src/app/nfc.c
@@ -0,0 +1,38 @@
+/*
+ * nfc.c
+ *
+ *  Created on: 2018Äê9ÔÂ17ÈÕ
+ *      Author: lifor
+ */
+
+#include "nfc.h"
+#include "spi.h"
+
+uint8_t write_reg(uint8_t *addr, uint8_t cammand)
+{
+	uint8_t uRet = 0x00;
+	return uRet;
+}
+
+uint8_t write_data(uint8_t *addr, uint8_t *data, uint8_t size)
+{
+	uint8_t uRet = 0x00;
+	return uRet;
+}
+
+
+void nfc_init()
+{
+
+}
+
+
+void nfc_task()
+{
+	nfc_init();
+	while(1)
+	{
+
+
+	}
+}
